0c8ddd6e68bd55ab5b3387fab016dbcf23b2a215
matijatomc <55134628+matijatomc@users.noreply.github.com>
2020-02-13 11:04:29 +0100

    Add files via upload

---
+++ tomcMatijaProvjeraZnanjaZadatak3/tomcMatijaProvjeraZnanjaZadatak3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomcMatijaProvjeraZnanjaZadatak3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi jednu rijec: ");
+            string text = Console.ReadLine();
+
+            for (int i = 1; i <= text.Length; i++)
+            {
+                for(int j = text.Length-i; j >0; j--)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(text.Substring( text.Length -i));
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
